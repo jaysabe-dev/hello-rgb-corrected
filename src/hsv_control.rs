858638c9d3e716e11744c05d2d849a0d68cf31b1
jaysabe <jsabegglen96@gmail.com>
2026-03-08 21:08:21 -0700

    removed TODO msg

--- src/hsv_control.rs
+++ src/hsv_control.rs
@@ -12,8 +12,6 @@ pub fn apply_selected_component(hsv: &mut Hsv, component: Component, knob_0_to_1
 }
 
 pub fn apply_hue(hsv: &mut Hsv, knob_0_to_1: f32) {
-    // TODO: Edit hue sequencing here. Replace this linear mapping with
-    // your custom hue order/steps as needed.
     hsv.h = knob_0_to_1 * HUE_MAX_TURNS;
 }
 
